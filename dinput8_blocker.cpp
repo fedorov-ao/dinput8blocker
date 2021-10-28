3c3dfc451b2aef1498daa09d4966a9fe723be961
fedorov-ao <fedorov-ao@yandex.ru>
2021-10-29 01:05:09 +0300

    Using decltype for imports.

--- dinput8_blocker.cpp
+++ dinput8_blocker.cpp
@@ -2,6 +2,7 @@
 #include <stdexcept>
 #include <fstream>
 #include <windows.h>
+#include <olectl.h>
 #define DIRECTINPUT_VERSION  0x0800
 #include <dinput.h>
 #pragma pack(1)
@@ -33,20 +34,11 @@ struct Imports
 {
   struct Dinput8
   {
-    typedef HRESULT WINAPI (DirectInput8Create_t)(HINSTANCE hinst, DWORD dwVersion, REFIID riidltf, LPVOID * ppvOut, LPUNKNOWN punkOuter);
-		DirectInput8Create_t * DirectInput8Create;
-
-    typedef HRESULT WINAPI (DllCanUnloadNow_t)();
-    DllCanUnloadNow_t * DllCanUnloadNow;
-
-    typedef HRESULT WINAPI (DllGetClassObject_t)(REFCLSID rclsid, REFIID riid, LPVOID *ppv);
-    DllGetClassObject_t * DllGetClassObject;
-
-    typedef HRESULT WINAPI (DllRegisterServer_t)();
-		DllRegisterServer_t * DllRegisterServer;
-    
-    typedef HRESULT WINAPI (DllUnregisterServer_t)();
-		DllUnregisterServer_t * DllUnregisterServer;
+    decltype(::DirectInput8Create) * DirectInput8Create;
+    decltype(::DllCanUnloadNow) * DllCanUnloadNow;
+    decltype(::DllGetClassObject) * DllGetClassObject;
+    decltype(::DllRegisterServer) * DllRegisterServer;
+    decltype(::DllUnregisterServer) * DllUnregisterServer;
   } dinput8;
 
   void fill();
@@ -67,11 +59,11 @@ void Imports::fill()
   if (!hNativeDinput8Dll) 
     throw std::runtime_error("Failed to get native dinput8.dll handle");
 
-  dinput8.DirectInput8Create = reinterpret_cast<Dinput8::DirectInput8Create_t *>(GetProcAddress(hNativeDinput8Dll,"DirectInput8Create"));
-  dinput8.DllCanUnloadNow = reinterpret_cast<Dinput8::DllCanUnloadNow_t *>(GetProcAddress(hNativeDinput8Dll,"DllCanUnloadNow"));
-  dinput8.DllGetClassObject = reinterpret_cast<Dinput8::DllGetClassObject_t *>(GetProcAddress(hNativeDinput8Dll,"DllGetClassObject"));
-  dinput8.DllRegisterServer = reinterpret_cast<Dinput8::DllRegisterServer_t *>(GetProcAddress(hNativeDinput8Dll,"DllRegisterServer"));
-  dinput8.DllUnregisterServer = reinterpret_cast<Dinput8::DllUnregisterServer_t *>(GetProcAddress(hNativeDinput8Dll,"DllUnregisterServer"));
+  dinput8.DirectInput8Create = reinterpret_cast<decltype(::DirectInput8Create) *>(GetProcAddress(hNativeDinput8Dll,"DirectInput8Create"));
+  dinput8.DllCanUnloadNow = reinterpret_cast<decltype(::DllCanUnloadNow) *>(GetProcAddress(hNativeDinput8Dll,"DllCanUnloadNow"));
+  dinput8.DllGetClassObject = reinterpret_cast<decltype(::DllGetClassObject) *>(GetProcAddress(hNativeDinput8Dll,"DllGetClassObject"));
+  dinput8.DllRegisterServer = reinterpret_cast<decltype(::DllRegisterServer) *>(GetProcAddress(hNativeDinput8Dll,"DllRegisterServer"));
+  dinput8.DllUnregisterServer = reinterpret_cast<decltype(::DllUnregisterServer) *>(GetProcAddress(hNativeDinput8Dll,"DllUnregisterServer"));
 
   log_debug("Filled imports");
   return;
@@ -80,19 +72,19 @@ void Imports::fill()
 
 BOOL WINAPI DllMain(HINSTANCE hInst, DWORD reason,LPVOID v)
 try {
-	if (reason == DLL_PROCESS_ATTACH)
-	{
+  if (reason == DLL_PROCESS_ATTACH)
+  {
     log_info("Dll attached");
     g_imports.fill();
-	}
+  }
   
   if (reason == DLL_PROCESS_DETACH)
-	{
+  {
     log_info("Dll detached");
-	}
+  }
 
   log_debug("DllMain success");
-	return TRUE;
+  return TRUE;
 } catch (std::exception const & e) 
 {
   log_error(e.what());
