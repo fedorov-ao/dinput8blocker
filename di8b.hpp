f405a65841aa1cfb71870f5a1bb6f10f71976cce
fedorov-ao <fedorov-ao@yandex.ru>
2022-01-05 16:04:03 +0300

    Moved helper functions declarations to di8b.hpp.

--- di8b.hpp
+++ di8b.hpp
@@ -21,6 +21,11 @@ namespace di8b
 UINT name2key(char const * name);
 char const * key2name(UINT key);
 
+size_t guid2cstr(char * buf, size_t n, REFGUID rguid);
+std::string guid2str(REFGUID rguid);
+
+std::string preset_guid2str(REFGUID rguid);
+
 enum class DeviceKind { mouse, keyboard, joystick, other };
 
 class Flag;
